1d071160e8b595141ba75e66c4ff79d151a67c96
Arash <Arash@172.30.33.83>
2014-09-08 21:53:54 -0400

    creatDir renamed to createDir

--- src/main/java/cloudservices/brokerage/commons/utils/file_utils/DirectoryUtil.java
+++ src/main/java/cloudservices/brokerage/commons/utils/file_utils/DirectoryUtil.java
@@ -12,7 +12,7 @@ import java.io.File;
  */
 public class DirectoryUtil {
 
-    public static boolean creatDir(String path) {
+    public static boolean createDir(String path) {
         File theDir = new File(path);
 
         // if the directory does not exist, create it
